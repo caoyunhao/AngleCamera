09eccb14adaaaaf64717e4130e946e74c634f29a
CaoYunhao <792775641@qq.com>
2016-12-05 12:47:11 +0800

    first commit

---
+++ app/src/main/java/com/myteam/anglecamera/MainActivity.java
@@ -0,0 +1,13 @@
+package com.myteam.anglecamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
